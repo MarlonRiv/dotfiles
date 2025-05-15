1e8d818b1a795ea757b39e052799ad3e8fe69cb2
Marlon Rivera <marlonriv90@gmail.com>
2025-05-15 19:17:03 +0200

    removed transparent backround from catppuccin

--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
 
     config = function()
       require("catppuccin").setup({
-        transparent_background = true,
+        --transparent_background = true,
       })
       vim.cmd.colorscheme "catppuccin"
     end
