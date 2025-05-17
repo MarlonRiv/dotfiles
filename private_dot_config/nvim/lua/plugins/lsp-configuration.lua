b5f15ffbb34c1ed6b850e816eeff3b403da5e894
Marlon Rivera <marlonriv90@gmail.com>
2025-05-17 15:06:47 +0200

    lsp configuration init

--- private_dot_config/nvim/lua/plugins/lsp-configuration.lua
+++ private_dot_config/nvim/lua/plugins/lsp-configuration.lua
@@ -1,7 +1,21 @@
 return {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
+    {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+        require("mason").setup()
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+    },
+    {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({})
+      end,
+    }
   }
