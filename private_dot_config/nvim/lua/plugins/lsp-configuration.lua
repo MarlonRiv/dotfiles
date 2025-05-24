b013a1e5814427119099a5ec2742283e269cfcdc
Marlon Rivera <marlonriv90@gmail.com>
2025-05-24 20:29:22 +0200

    Automatic LSP installation

--- private_dot_config/nvim/lua/plugins/lsp-configuration.lua
+++ private_dot_config/nvim/lua/plugins/lsp-configuration.lua
@@ -1,185 +1,318 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   lazy = false,
+  -- },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install these LSP servers
+        ensure_installed = {
+          "clangd", -- C++ (for ROS nodes)
+          "pylsp", -- Python (for ROS scripts)
+          "lemminx", -- XML (for launch files, URDF)
+          "yamlls", -- YAML (for parameter files)
+          "lua_ls", -- Lua (for Neovim config)
+          "bashls", -- Bash scripts
+          "cmake", -- CMake files
+          "jsonls", -- JSON files
+        },
+        -- Automatically configure installed servers
+        handlers = {
+          -- Default handler - applies to all servers without custom config
+          function(server_name)
+            require("lspconfig")[server_name].setup({
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            })
+          end,
 
-			-- ROS
+          -- Custom handlers for servers that need special configuration
+          ["clangd"] = function()
+            require("lspconfig").clangd.setup({
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+              cmd = {
+                "clangd",
+                "--background-index",
+                "--clang-tidy",
+                "--header-insertion=iwyu",
+                "--completion-style=detailed",
+                "--function-arg-placeholders",
+                "--fallback-style=llvm",
+                "--compile-commands-dir=build",
+              },
+              init_options = {
+                usePlaceholders = true,
+                completeUnimported = true,
+                clangdFileStatus = true,
+              },
+              root_dir = function(fname)
+                local util = require("lspconfig.util")
+                return util.root_pattern(
+                  "compile_commands.json",
+                  "compile_flags.txt",
+                  ".git",
+                  "devel",
+                  "build",
+                  "install"
+                )(fname) or util.path.dirname(fname)
+              end,
+            })
+          end,
 
-			-- ROS XML files (launch files, URDF, etc.)
-			lspconfig.lemminx.setup({
-				capabilities = capabilities,
-				settings = {
-					xml = {
-						catalogs = {
-							-- Add ROS XML schemas
-							vim.fn.expand("~/.local/share/nvim/ros_schemas.xml"),
-						},
-						validation = {
-							enabled = true,
-						},
-						completion = {
-							autoCloseTags = true,
-						},
-					},
-				},
-			})
+          ["pylsp"] = function()
+            require("lspconfig").pylsp.setup({
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+              settings = {
+                pylsp = {
+                  plugins = {
+                    pycodestyle = {
+                      ignore = { "W391" },
+                      maxLineLength = 100,
+                    },
+                    rope_completion = {
+                      enabled = true,
+                    },
+                    jedi_completion = {
+                      enabled = true,
+                      include_params = true,
+                    },
+                    jedi_hover = {
+                      enabled = true,
+                    },
+                    jedi_references = {
+                      enabled = true,
+                    },
+                    jedi_signature_help = {
+                      enabled = true,
+                    },
+                    jedi_symbols = {
+                      enabled = true,
+                      all_scopes = true,
+                    },
+                  },
+                },
+              },
+            })
+          end,
 
-			-- Enhanced Python with ROS-specific configuration
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-				settings = {
-					pylsp = {
-						plugins = {
-							pycodestyle = {
-								ignore = { "W391" },
-								maxLineLength = 100,
-							},
-							rope_completion = {
-								enabled = true,
-							},
-							jedi_completion = {
-								enabled = true,
-								include_params = true,
-							},
-							jedi_hover = {
-								enabled = true,
-							},
-							jedi_references = {
-								enabled = true,
-							},
-							jedi_signature_help = {
-								enabled = true,
-							},
-							jedi_symbols = {
-								enabled = true,
-								all_scopes = true,
-							},
-						},
-					},
-				},
-			})
+          ["lemminx"] = function()
+            require("lspconfig").lemminx.setup({
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+              settings = {
+                xml = {
+                  catalogs = {
+                    vim.fn.expand("~/.local/share/nvim/ros_schemas.xml"),
+                  },
+                  validation = {
+                    enabled = true,
+                  },
+                  completion = {
+                    autoCloseTags = true,
+                  },
+                },
+              },
+            })
+          end,
 
-			-- Enhanced C++ with ROS-specific configuration
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				cmd = {
-					"clangd",
-					"--background-index",
-					"--clang-tidy",
-					"--header-insertion=iwyu",
-					"--completion-style=detailed",
-					"--function-arg-placeholders",
-					"--fallback-style=llvm",
-					"--compile-commands-dir=build", -- Look for compile_commands.json in build dir
-				},
-				init_options = {
-					usePlaceholders = true,
-					completeUnimported = true,
-					clangdFileStatus = true,
-				},
-				root_dir = function(fname)
-					-- Look for ROS workspace indicators
-					local util = require("lspconfig.util")
-					return util.root_pattern(
-						"compile_commands.json",
-						"compile_flags.txt",
-						".git",
-						"devel",
-						"build",
-						"install"
-					)(fname) or util.path.dirname(fname)
-				end,
-			})
+          ["yamlls"] = function()
+            require("lspconfig").yamlls.setup({
+              capabilities = require("cmp_nvim_lsp").default_capabilities(),
+              settings = {
+                yaml = {
+                  schemas = {
+                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+                    "docker-compose*.yml",
+                  },
+                  validate = true,
+                  completion = true,
+                },
+              },
+            })
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      -- ROS
 
-			-- Configure diagnostics
-			vim.diagnostic.config({
-				virtual_text = false, -- Disable inline text to avoid clutter
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = false,
-				float = {
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-					focusable = false,
-				},
-			})
+      -- ROS XML files (launch files, URDF, etc.)
+      -- lspconfig.lemminx.setup({
+      --   capabilities = capabilities,
+      --   settings = {
+      --     xml = {
+      --       catalogs = {
+      --         -- Add ROS XML schemas
+      --         vim.fn.expand("~/.local/share/nvim/ros_schemas.xml"),
+      --       },
+      --       validation = {
+      --         enabled = true,
+      --       },
+      --       completion = {
+      --         autoCloseTags = true,
+      --       },
+      --     },
+      --   },
+      -- })
 
-			-- Set updatetime for faster CursorHold trigger
-			vim.o.updatetime = 250
+      -- Enhanced Python with ROS-specific configuration
+      -- lspconfig.pylsp.setup({
+      --   capabilities = capabilities,
+      --   settings = {
+      --     pylsp = {
+      --       plugins = {
+      --         pycodestyle = {
+      --           ignore = { "W391" },
+      --           maxLineLength = 100,
+      --         },
+      --         rope_completion = {
+      --           enabled = true,
+      --         },
+      --         jedi_completion = {
+      --           enabled = true,
+      --           include_params = true,
+      --         },
+      --         jedi_hover = {
+      --           enabled = true,
+      --         },
+      --         jedi_references = {
+      --           enabled = true,
+      --         },
+      --         jedi_signature_help = {
+      --           enabled = true,
+      --         },
+      --         jedi_symbols = {
+      --           enabled = true,
+      --           all_scopes = true,
+      --         },
+      --       },
+      --     },
+      --   },
+      -- })
 
-			-- Auto-show diagnostics on cursor hold
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				callback = function()
-					vim.diagnostic.open_float(nil, { focus = false })
-				end,
-			})
+      -- Enhanced C++ with ROS-specific configuration
+      -- lspconfig.clangd.setup({
+      --   capabilities = capabilities,
+      --   cmd = {
+      --     "clangd",
+      --     "--background-index",
+      --     "--clang-tidy",
+      --     "--header-insertion=iwyu",
+      --     "--completion-style=detailed",
+      --     "--function-arg-placeholders",
+      --     "--fallback-style=llvm",
+      --     "--compile-commands-dir=build", -- Look for compile_commands.json in build dir
+      --   },
+      --   init_options = {
+      --     usePlaceholders = true,
+      --     completeUnimported = true,
+      --     clangdFileStatus = true,
+      --   },
+      --   root_dir = function(fname)
+      --     -- Look for ROS workspace indicators
+      --     local util = require("lspconfig.util")
+      --     return util.root_pattern(
+      --       "compile_commands.json",
+      --       "compile_flags.txt",
+      --       ".git",
+      --       "devel",
+      --       "build",
+      --       "install"
+      --     )(fname) or util.path.dirname(fname)
+      --   end,
+      -- })
 
-			lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
-			-- lspconfig.clangd.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cmake.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			-- lspconfig.yamlls.setup({
-			-- 	capabilities = capabilities,
-			-- })
+      -- Configure diagnostics
+      vim.diagnostic.config({
+        virtual_text = false, -- Disable inline text to avoid clutter
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = false,
+        float = {
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+          focusable = false,
+        },
+      })
 
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-				settings = {
-					yaml = {
-						schemas = {
-							-- Add ROS parameter file schemas
-							["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.yml",
-						},
-						validate = true,
-						completion = true,
-					},
-				},
-			})
-			-- lspconfig.pylsp.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			-- LSP Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+      -- Set updatetime for faster CursorHold trigger
+      vim.o.updatetime = 250
 
-			-- Additional diagnostic keymaps
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
-		end,
-	},
+      -- Auto-show diagnostics on cursor hold
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end,
+      })
+
+      lspconfig.lua_ls.setup({})
+
+      -- lspconfig.clangd.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cmake.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      -- lspconfig.yamlls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      --
+      -- lspconfig.yamlls.setup({
+      --   capabilities = capabilities,
+      --   settings = {
+      --     yaml = {
+      --       schemas = {
+      --         -- Add ROS parameter file schemas
+      --         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+      --         "docker-compose*.yml",
+      --       },
+      --       validate = true,
+      --       completion = true,
+      --     },
+      --   },
+      -- })
+      -- lspconfig.pylsp.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      -- LSP Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+
+      -- Additional diagnostic keymaps
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+    end,
+  },
 }
